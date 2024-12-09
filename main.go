f04d3b5d6a961fda996480465b14edbb27b57b73
markson1998 <49343245+markson1998@users.noreply.github.com>
2024-12-09 18:12:04 +0000

    Config + Logger + Sqlite + Handlers

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/marksonw/go-vacancies/router"
+import (
+	"github.com/marksonw/go-vacancies/config"
+	"github.com/marksonw/go-vacancies/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+	// Initialize Configuration
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("configuration initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
