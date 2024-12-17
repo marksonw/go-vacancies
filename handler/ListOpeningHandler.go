01eba0af734347c62fc2f9e88208ce2afddb01eb
markson1998 <49343245+markson1998@users.noreply.github.com>
2024-12-17 22:27:58 +0000

    feat: List Opening Handler Implementation

--- handler/ListOpeningHandler.go
+++ handler/ListOpeningHandler.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/marksonw/go-vacancies/schemas"
 )
 
 func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listening openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
