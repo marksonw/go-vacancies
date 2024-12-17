843bb6dfc2dd4397704ee5810c97d66c68f37c1e
markson1998 <49343245+markson1998@users.noreply.github.com>
2024-12-17 22:32:57 +0000

    feat: Update Opening Handler & requests/responses

--- router/routes.go
+++ router/routes.go
@@ -12,7 +12,7 @@ func initializeroutes(router *gin.Engine) {
 
 	v1 := router.Group("/api/v1/")
 	{
-		v1.GET("/opening", handler.ListOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
