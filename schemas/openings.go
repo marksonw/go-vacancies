16673c909db71887adfd89b19f40db070693baf3
markson1998 <49343245+markson1998@users.noreply.github.com>
2024-12-18 04:01:53 +0000

    feat: Adding UUID as primary key within the database schema

--- schemas/openings.go
+++ schemas/openings.go
@@ -3,21 +3,25 @@ package schemas
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Opening struct {
-	gorm.Model
-	Role     string
-	Company  string
-	Location string
-	Remote   bool
-	Link     string
-	Salary   int64
+	ID        uuid.UUID `gorm:"type:text;primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"` // Soft delete support
+	Role      string
+	Company   string
+	Location  string
+	Remote    bool
+	Link      string
+	Salary    int64
 }
 
 type OpeningResponse struct {
-	ID        uint      `json:"id"`
+	ID        string    `json:"id"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 	DeletedAt time.Time `json:"deteledAt,omitempty"`
@@ -27,4 +31,11 @@ type OpeningResponse struct {
 	Remote    bool      `json:"remote"`
 	Link      string    `json:"link"`
 	Salary    int64     `json:"salary"`
+}
+
+func (opening *Opening) BeforeCreate(tx *gorm.DB) (err error) {
+	if opening.ID == uuid.Nil {
+		opening.ID = uuid.New() // Generate UUID in Go
+	}
+	return
 }
